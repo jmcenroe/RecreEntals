d8b3469a8c34cbb4bf39693ec1d5e32fe329cf4f
Jarrod McEnroe <jwmcenroe@gmail.com>
2018-02-06 19:28:30 -0800

    Played w/routes and server connections

--- models/index.js
+++ models/index.js
@@ -4,9 +4,11 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
+var env       = process.env.DATABASE_URL || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
+
+console.log(env);
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
@@ -21,6 +23,7 @@ fs
   })
   .forEach(file => {
     var model = sequelize['import'](path.join(__dirname, file));
+
     db[model.name] = model;
   });
 
