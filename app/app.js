50e5229a1093ff0b422559902b80fa8e7370dcf9
cclarkcode <cclarkrun@gmail.com>
2018-02-08 18:39:45 -0800

    I want to pull master

--- app/app.js
+++ app/app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+// var passport = require('./modules/passport/');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var db = require('./modules/models');
+var seed = require('./seeders/seeds');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.use(passport);
 
 app.use('/', index);
 app.use('/users', users);
@@ -32,6 +37,8 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -41,6 +48,20 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+db.sequelize.sync({
+  force: true
+})
+//Run seed functions to populate database
+.then(function () {
+  var promise = seed(db);
+  console.log('This is our promise:', promise);
+  promise.then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
 });
 
 module.exports = app;
