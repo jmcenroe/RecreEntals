1402f1dd942e3a036cf4f175d705f51aef820db5
torismith <victoriaashleysmith@gmail.com>
2018-02-13 21:21:27 -0800

    bootstrap linked up

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -5,13 +5,17 @@ import logo from '../assets/img/recre-entals-black.gif';
 class About extends Component{
     render(){
         return(
-            <div>
-            <h1>About the RecreEntals Team</h1>
-            <h4>Chris Clark</h4>
-            <h4>Daniel John</h4>
-            <h4>Jarrod McEnroe</h4>
-            <h4>Tori Smith</h4>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="col-lg"><h4>Chris Clark</h4></div>
+                        <div className="col-lg"><h4>Daniel John</h4></div>
+                        <div className="col-lg"><h4>Jarrod McEnroe</h4></div>
+                        <div className="col-lg"><h4>Tori Smith</h4></div>
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
