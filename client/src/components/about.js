cff52cc017d99cd2583d02d211e993bc4fb40b32
torismith <victoriaashleysmith@gmail.com>
2018-02-13 20:38:38 -0800

    ughhhhh

---
+++ client/src/components/about.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class About extends Component{
+    render(){
+        return(
+            <h1>About Us</h1>
+            </div>
+        );
+    }
+}
+
+export default About;
