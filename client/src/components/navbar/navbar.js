87afc0dcb4941520e98b22fae5b26c2ca7cbe574
drj613 <drj613@gmail.com>
2018-02-19 11:51:22 -0800

    styling of navbar (should we make it fullscreen?)

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
+import Row from "../row";
 import "./navbar.css";
-
+import Col from "../col";
+import logo from "../../assets/img/recre-entals-white.gif";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 class Navbar extends Component {
@@ -27,13 +29,16 @@ componentDidMount() {
    
     return <nav className="navbar navbar-default">
     <div className="container-fluid">
-      
+      <Row>
+        <Col size="3">
           <div className="navbar-header">
             <span className="navbar-brand">
-              NYT Search and Save
+              <img src={logo} id="logo"/>
             </span>
           </div>
-          <ul className="nav navbar-nav">
+        </Col>
+        <Col size="3" className="col-3 offset-sm-6 offset-md-6 offset-lg-6" id="options">
+          <ul className="navbar-nav">
             <li
               className={
                 window.location.pathname === "/"
@@ -56,9 +61,10 @@ componentDidMount() {
             </li>
             
                 
-          </ul>  
+          </ul> 
+        </Col> 
         
-        
+        </Row>
     </div>
   </nav>;
 
