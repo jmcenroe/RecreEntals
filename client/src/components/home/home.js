ee62ed8ac0b2c12ee5ead193222066fd05f768ee
drj613 <drj613@gmail.com>
2018-02-21 11:18:47 -0800

    some file structure, tweaking of wrapper

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -5,7 +5,7 @@ import './home.css';
 class Home extends Component{
     render(){
         return(
-            <div className>
+            <div className="container-fluid">
                 <h2>Browse by Category</h2>
                 <h3>Small Sporting Goods</h3>
                 <p><i className="fas fa-football-ball"></i></p>
