79c8af32eac1245540cf1036656375835e3fef6e
cclarkcode <cclarkrun@gmail.com>
2018-02-22 11:59:08 -0800

    heroku

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ app.use(session({ secret: 'secret'}));
 app.use(passport.initialize());
 app.use(passport.session());
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
