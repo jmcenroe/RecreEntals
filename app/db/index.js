6f31eabf481a026342aac422db8266af6580adb5
cclarkcode <cclarkrun@gmail.com>
2018-02-22 11:44:11 -0800

    working on heroku

--- app/db/index.js
+++ app/db/index.js
@@ -9,7 +9,7 @@ var config    = require(__dirname + '/config.json')[env];
 var db        = {};
 
 console.log(env);
-console.log(__dirname + '/../../config/config.json');
+console.log(__dirname + '/config.json');
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
